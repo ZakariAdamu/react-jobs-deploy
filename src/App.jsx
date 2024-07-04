a3633a2e885057e2ff4380631e3c7cc4a5a95569
Zakari Adamu <ojonimizakari@gmail.com>
2024-07-04 12:02:59 +0100

    feat: setup the addJob functionality via a POST request, and the deleteJob functionality via a DELETE request respectively

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,45 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import JobPage, {jobLoader} from "./pages/JobPage";
-
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<MainLayout />}>
-			<Route index element={<HomePage />} />
-			<Route path="/jobs" element={<JobsPage />} />
-			<Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-			<Route path="*" element={<NotFoundPage />} />
-		</Route>
-	)
-);
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+	// Add new job (send a post requet to the server)
+	const addJob = async (newJob) => {
+		const res = await fetch("/api/jobs", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newJob),
+		});
+		return;
+	};
+
+	// Delete job (send a delete request to the server)
+
+	const deleteJob = async (id) => {
+		const res = await fetch(`/api/jobs/${id}`, {
+			method: "DELETE",
+		});
+	};
+
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<MainLayout />}>
+				<Route index element={<HomePage />} />
+				<Route path="/jobs" element={<JobsPage />} />
+				<Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+				<Route
+					path="/jobs/:id"
+					element={<JobPage deleteJob={deleteJob} />}
+					loader={jobLoader}
+				/>
+				<Route path="*" element={<NotFoundPage />} />
+			</Route>
+		)
+	);
 	return <RouterProvider router={router} />;
 };
 
