d858f98bf02249c3d2ff09b780b47dc27b08bcc5
Zakari Adamu <ojonimizakari@gmail.com>
2024-08-02 11:16:15 +0100

    npm i vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://react-jobs-deploy-be.vercel.app/api/jobs",
+				target: "http://localhost:5002/api",
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
 			},
