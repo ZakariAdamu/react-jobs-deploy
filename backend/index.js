d9385c441b836a09eebc7258ae280fc10da50203
Zakari Adamu <ojonimizakari@gmail.com>
2024-08-03 11:59:42 +0100

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const jobRoute = require("./routes/job.route");
 const app = express();
 
+app.use(
+	cors({
+		origin: ["https://react-jobs-deploy-client.vercel.app"],
+		methods: ["POST", "GET"],
+		credentials: true,
+	})
+);
+
 app.use(express.json());
+
+mongoose
+	.connect(
+		"mongodb+srv://zakariadamu714:pc3bQHH0okMD0a4j@cluster0.yxnuqu4.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0"
+	)
+	.then(() => {
+		console.log("Connected to Database!!");
+	})
+	.catch(() => console.log("Connection failed!"));
 
 // middleware configuration to allow for entering data in other formats (eg. form encoded format) aside the JSON format
 app.use(express.urlencoded({ extended: false }));
-// app.use(cors());
-// DeviceMotionEvent.config({ path: "./config/.env" });
 
 // routes
 app.use("/api/jobs", jobRoute);
 
-const port = process.env.PORT || 5002;
-
-app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port} Enjoy coding!`);
-});
-
 // Get backend api homepage (initial url testing)
-// app.get("/", (req, res) => {
-// 	res.send("Hello from Node Backend API, AlhamdulilLah");
-// });
+
 app.get("/", (req, res) => {
 	res.send("Hello from Node Backend API, AlhamdulilLah");
 });
@@ -35,11 +43,8 @@ app.get("/", (req, res) => {
 // 	})
 // 	.catch(() => console.log("Connection failed!"));
 
-mongoose
-	.connect(
-		"mongodb+srv://zakariadamu714:pc3bQHH0okMD0a4j@cluster0.yxnuqu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-	)
-	.then(() => {
-		console.log("Connected to Database!!");
-	})
-	.catch(() => console.log("Connection failed!"));
+// ====== Second trial setup ========
+
+app.listen(5002, () => {
+	console.log("Server is running at http://localhost:5002 Enjoy coding!");
+});
