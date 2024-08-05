12bb433f9cf13713ed36a2f3e4d72fb7aead4c07
Zakari Adamu <53929284+ZakariAdamu@users.noreply.github.com>
2024-08-05 09:54:44 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://react-jobs-deploy-api.vercel.app/api",
+				target: "https://react-jobs-deploy-api.vercel.app",
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
 			},
